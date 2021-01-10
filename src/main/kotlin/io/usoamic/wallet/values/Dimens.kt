e542c6eaf3f782765ddc79c30f9110c3596d33ac
primys <a2uso@tuta.io>
2021-01-10 14:51:27 -0800

    Remove dp

--- src/main/kotlin/io/usoamic/wallet/values/Dimens.kt
+++ src/main/kotlin/io/usoamic/wallet/values/Dimens.kt
@@ -1,9 +1,7 @@
 package io.usoamic.wallet.values
 
-import androidx.compose.ui.unit.dp
-
 object Dimens {
-    val DEFAULT_INDENT = 10.dp
-    val DEFAULT_INDENT2 = 20.dp
-    val DEFAULT_INDENT3 = 25.dp
+    val DEFAULT_INDENT = 10
+    val DEFAULT_INDENT2 = 20
+    val DEFAULT_INDENT3 = 25
 }
