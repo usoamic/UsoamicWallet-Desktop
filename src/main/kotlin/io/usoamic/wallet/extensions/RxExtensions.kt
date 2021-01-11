c2b5e9837dbd95b755f5a1391c1cd2115f08ad7a
primys <a2uso@tuta.io>
2021-01-10 18:12:53 -0800

    Fix observeOnMain()

--- src/main/kotlin/io/usoamic/wallet/extensions/RxExtensions.kt
+++ src/main/kotlin/io/usoamic/wallet/extensions/RxExtensions.kt
@@ -2,11 +2,12 @@ package io.usoamic.wallet.extensions
 
 import io.reactivex.Completable
 import io.reactivex.Single
+import io.reactivex.rxjavafx.schedulers.JavaFxScheduler
 import io.reactivex.schedulers.Schedulers
 import io.usoamic.wallet.util.BuildConfig
 import java.util.concurrent.TimeUnit
 
-fun <T> Single<T>.observeOnMain(): Single<T> = observeOn(Schedulers.single())
+fun <T> Single<T>.observeOnMain(): Single<T> = observeOn(JavaFxScheduler.platform())
 
 fun <T> Single<T>.subscribeOnIo(): Single<T> = subscribeOn(Schedulers.io())
 
