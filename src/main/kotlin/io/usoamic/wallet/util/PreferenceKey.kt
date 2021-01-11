853e655b3b05782b8471d2e1561c2da6e999c8e2
primys <a2uso@tuta.io>
2021-01-10 16:50:34 -0800

    Add utils

---
+++ src/main/kotlin/io/usoamic/wallet/util/PreferenceKey.kt
@@ -0,0 +1,7 @@
+package io.usoamic.wallet.util
+
+object PreferenceKey {
+    const val SHARED_PREFS: String = "sharedPrefs"
+    const val TIMESTAMP: String = "date"
+    const val ADDRESS: String = "address"
+}
