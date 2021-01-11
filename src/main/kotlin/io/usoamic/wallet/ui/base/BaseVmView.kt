23db8069178c33d0b61fccd94d14913b26108245
primys <a2uso@tuta.io>
2021-01-11 15:49:07 -0800

    Set APP_NAME as default window title

--- src/main/kotlin/io/usoamic/wallet/ui/base/BaseVmView.kt
+++ src/main/kotlin/io/usoamic/wallet/ui/base/BaseVmView.kt
@@ -7,7 +7,7 @@ import io.usoamic.wallet.values.R
 import javafx.scene.Node
 import javax.inject.Inject
 
-abstract class BaseVmView<T : BaseViewModel>(title: String? = null, icon: Node? = null) : BaseView(title, icon) {
+abstract class BaseVmView<T : BaseViewModel>(title: String = R.string.APP_NAME, icon: Node? = null) : BaseView(title, icon) {
     @Inject
     lateinit var viewModel: T
 
