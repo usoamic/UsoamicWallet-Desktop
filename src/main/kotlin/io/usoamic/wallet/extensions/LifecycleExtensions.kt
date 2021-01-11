9fcbb4b22b2cae02f527acecd8c5ad35a69f3759
primys <a2uso@tuta.io>
2021-01-10 18:12:42 -0800

    Add extensions for MutableLiveData

---
+++ src/main/kotlin/io/usoamic/wallet/extensions/LifecycleExtensions.kt
@@ -0,0 +1,20 @@
+package io.usoamic.wallet.extensions
+
+import io.usoamic.wallet.other.MutableLiveData
+import tornadofx.onChange
+
+fun <T> observe(ld: MutableLiveData<T>, callback: (T) -> Unit) {
+    ld.onChange {
+        if(it != null) {
+            callback.invoke(it)
+        }
+    }
+}
+
+fun MutableLiveData<Unit>.emit() {
+    value = Unit
+}
+
+fun <T> MutableLiveData<T>.emit(arg: T) {
+    value = arg
+}
