ddd9fc5422eb1cce85678b27d139bab4ca13a16e
primys <a2uso@tuta.io>
2021-01-11 16:16:50 -0800

    Implemented StartView

--- src/main/kotlin/io/usoamic/wallet/ui/start/StartView.kt
+++ src/main/kotlin/io/usoamic/wallet/ui/start/StartView.kt
@@ -1,26 +1,60 @@
 package io.usoamic.wallet.ui.start
 
 import io.usoamic.wallet.UsoamicWallet
-import io.usoamic.wallet.other.Styles
+import io.usoamic.wallet.extensions.image
+import io.usoamic.wallet.extensions.observe
+import io.usoamic.wallet.ui.auth.auth.AuthView
 import io.usoamic.wallet.ui.base.BaseVmView
-import javafx.scene.layout.HBox
+import io.usoamic.wallet.ui.main.wallet.WalletView
+import io.usoamic.wallet.util.BuildConfig
+import io.usoamic.wallet.values.R
+import javafx.geometry.Pos
+import javafx.scene.layout.StackPane
 import tornadofx.*
 
-class StartView : BaseVmView<StartViewModel>("Hello TornadoFX") {
-    override val root: HBox = hbox {
-        label(viewModel.idProperty) {
-            addClass(Styles.heading)
-            onLeftClick {
-                viewModel.run()
+class StartView : BaseVmView<StartViewModel>() {
+    override val root: StackPane = stackpane {
+        paddingAll = 50
+        vbox {
+            vbox {
+                alignment = Pos.CENTER
+                imageview {
+                    image = image(R.image.IC_USOAMIC)
+                    fitHeight = 200.0
+                    fitWidth = 200.0
+                }
+                label(R.string.APP_NAME)
+                label {
+                    text = StringBuilder()
+                        .append(BuildConfig.VERSION_NAME)
+                        .append(" (")
+                        .append(BuildConfig.VERSION_CODE)
+                        .append(")")
+                        .toString()
+                }
+                progressbar {
+                    paddingTop = 10
+                }
             }
-        }
-        stackpane {
-            visibleWhen { viewModel.ldProgress }
-            progressindicator()
+
         }
     }
 
     override fun inject() {
         UsoamicWallet.component.inject(this)
+    }
+
+    override fun initObservers() {
+        super.initObservers()
+        observe(viewModel.ldHasAccount, ::goToNextView)
+    }
+
+
+    private fun goToNextView(hasAccount: Boolean) {
+        if (hasAccount) {
+            replaceWith<WalletView>()
+        } else {
+            replaceWith<AuthView>()
+        }
     }
 }
--- src/main/kotlin/io/usoamic/wallet/ui/start/StartViewModel.kt
+++ src/main/kotlin/io/usoamic/wallet/ui/start/StartViewModel.kt
@@ -1,30 +1,26 @@
 package io.usoamic.wallet.ui.start
 
-import io.usoamic.wallet.domain.repositories.EthereumRepository
-import io.usoamic.wallet.extensions.observeOnMain
-import io.usoamic.wallet.extensions.subscribeOnIo
+import io.usoamic.wallet.extensions.addSchedulers
 import io.usoamic.wallet.other.MutableLiveData
 import io.usoamic.wallet.ui.base.BaseViewModel
 import io.usoamic.wallet.usecases.StartUseCases
 import javax.inject.Inject
 
 class StartViewModel @Inject constructor(
-    private val mStartUseCases: StartUseCases,
-    private val mEthereumRepository: EthereumRepository
+    private val mStartUseCases: StartUseCases
 ) : BaseViewModel() {
-    val idProperty = MutableLiveData<Int>(0)
+    val ldHasAccount = MutableLiveData<Boolean>()
 
-    fun run() {
-        //Fix this, return only old variable
-        mEthereumRepository.ethHeight
-            .subscribeOnIo()
-            .observeOnMain()
-            .addProgress()
+    init {
+        checkAccount()
+    }
+
+    private fun checkAccount() {
+        mStartUseCases.hasAccount()
+            .addSchedulers()
             .subscribe({
-                idProperty.value = it.toInt()
-            }, {
-                it.printStackTrace()
-            })
+                ldHasAccount.value = it
+            }, ::throwErrorAndFinish)
             .addToDisposable()
     }
 }
