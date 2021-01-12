be6c38a08f4b77bc9f65bb70180c7ebdcea83c4e
primys <a2uso@tuta.io>
2021-01-11 16:23:31 -0800

    Add replaceWithSlideLeft extension

--- src/main/kotlin/io/usoamic/wallet/extensions/ComponentExtensions.kt
+++ src/main/kotlin/io/usoamic/wallet/extensions/ComponentExtensions.kt
@@ -2,7 +2,20 @@ package io.usoamic.wallet.extensions
 
 import javafx.scene.image.Image
 import tornadofx.Component
+import tornadofx.UIComponent
+import tornadofx.ViewTransition
+import tornadofx.seconds
 
 fun Component.image(url: String): Image {
     return resources.image(url)
+}
+
+inline fun <reified T : UIComponent> UIComponent.replaceWithSlideLeft(): Boolean {
+    return replaceWith(
+        T::class,
+        ViewTransition.Slide(
+            0.3.seconds,
+            ViewTransition.Direction.LEFT
+        )
+    )
 }
