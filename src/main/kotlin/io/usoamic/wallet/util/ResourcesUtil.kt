a8b7b22195eb6699d2e382033125075b04aaaf4b
primys <a2uso@tuta.io>
2021-01-11 16:17:55 -0800

    Now to getImage passing path

--- src/main/kotlin/io/usoamic/wallet/util/ResourcesUtil.kt
+++ src/main/kotlin/io/usoamic/wallet/util/ResourcesUtil.kt
@@ -3,11 +3,9 @@ package io.usoamic.wallet.util
 import javafx.scene.image.Image
 
 object ResourcesUtil {
-    private const val imageSrc = "/images"
-
     @JvmStatic
-    fun getImage(name: String, backgroundLoading: Boolean = false): Image {
-        val url = javaClass.getResource("$imageSrc/$name")
+    fun getImage(path: String, backgroundLoading: Boolean = false): Image {
+        val url = javaClass.getResource(path)
         return Image(url.toString(), backgroundLoading)
     }
 }
