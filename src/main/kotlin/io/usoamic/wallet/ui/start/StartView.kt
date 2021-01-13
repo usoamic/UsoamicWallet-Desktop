e9c2c73f69f9e8db74fa6363801f07b676baec49
primys <a2uso@tuta.io>
2021-01-13 08:24:17 -0800

    Some minor refactoring

--- src/main/kotlin/io/usoamic/wallet/ui/start/StartView.kt
+++ src/main/kotlin/io/usoamic/wallet/ui/start/StartView.kt
@@ -17,23 +17,28 @@ class StartView : BaseVmView<StartViewModel>() {
         paddingAll = 50
         vbox {
             vbox {
+                spacing = 10.0
                 alignment = Pos.CENTER
-                imageview {
-                    image = image(R.image.IC_USOAMIC)
+                imageview(R.image.IC_USOAMIC) {
                     fitHeight = 200.0
                     fitWidth = 200.0
                 }
-                label(R.string.APP_NAME)
-                label {
-                    text = StringBuilder()
-                        .append(BuildConfig.VERSION_NAME)
-                        .append(" (")
-                        .append(BuildConfig.VERSION_CODE)
-                        .append(")")
-                        .toString()
+                vbox {
+                    alignment = Pos.CENTER
+                    label(R.string.APP_NAME)
+                    label {
+                        text = StringBuilder()
+                            .append(BuildConfig.VERSION_NAME)
+                            .append(" (")
+                            .append(BuildConfig.VERSION_CODE)
+                            .append(")")
+                            .toString()
+                    }
                 }
                 progressbar {
-                    paddingTop = 10
+                    vboxConstraints {
+                        marginTop = 10.0
+                    }
                 }
             }
 
@@ -48,7 +53,6 @@ class StartView : BaseVmView<StartViewModel>() {
         super.initObservers()
         observe(viewModel.ldHasAccount, ::goToNextView)
     }
-
 
     private fun goToNextView(hasAccount: Boolean) {
         if (hasAccount) {
