de7e92d98a3ee07aa551c1a0b62a69a5c733348e
primys <a2uso@tuta.io>
2021-01-14 10:39:36 -0800

    Add AddView

--- src/main/kotlin/io/usoamic/wallet/di/subcomponents/add/AddSubcomponent.kt
+++ src/main/kotlin/io/usoamic/wallet/di/subcomponents/add/AddSubcomponent.kt
@@ -2,12 +2,14 @@ package io.usoamic.wallet.di.subcomponents.add
 
 import dagger.Subcomponent
 import io.usoamic.wallet.di.scopes.AuthScope
+import io.usoamic.wallet.ui.auth.add.AddView
+
 //import io.usoamic.wallet.ui.auth.add.AddFragment
 
 @AuthScope
 @Subcomponent
 interface AddSubcomponent {
-   // fun inject(addFragment: AddFragment)
+    fun inject(addFragment: AddView)
 
     @Subcomponent.Factory
     interface Factory {
---
+++ src/main/kotlin/io/usoamic/wallet/ui/auth/add/AddView.kt
@@ -0,0 +1,67 @@
+package io.usoamic.wallet.ui.auth.add
+
+import io.usoamic.wallet.UsoamicWallet
+import io.usoamic.wallet.extensions.backbuttontoolbar
+import io.usoamic.wallet.extensions.observe
+import io.usoamic.wallet.extensions.replaceWithSlideLeft
+import io.usoamic.wallet.extensions.replaceWithSlideRight
+import io.usoamic.wallet.ui.auth.auth.AuthView
+import io.usoamic.wallet.ui.auth.unlock.UnlockView
+import io.usoamic.wallet.ui.base.BaseVmView
+import io.usoamic.wallet.values.R
+import javafx.scene.layout.StackPane
+import tornadofx.*
+
+class AddView : BaseVmView<AddViewModel>() {
+    private val propPrivateKey = stringProperty("")
+    private val propPassword = stringProperty("")
+    private val propConfirmPassword = stringProperty("")
+
+    override val root: StackPane = stackpane {
+        progressbar {
+            visibleWhen { viewModel.ldProgress }
+        }
+        vbox {
+            hiddenWhen { viewModel.ldProgress }
+            backbuttontoolbar {
+                replaceWithSlideRight<AuthView>()
+            }
+            vbox {
+                form {
+                    fieldset {
+                        field(R.string.PRIVATE_KEY) {
+                            textfield(propPrivateKey)
+                        }
+                        field(R.string.PASSWORD) {
+                            textfield(propPassword)
+                        }
+                        field(R.string.CONFIRM_PASSWORD) {
+                            textfield(propConfirmPassword)
+                        }
+                    }
+                    button(R.string.ADD_ACCOUNT) {
+                        fitToParentWidth()
+                        action {
+                            viewModel.onAddClick(propPrivateKey.value, propPassword.value, propConfirmPassword.value)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun inject() {
+        UsoamicWallet.component.addSubcomponent.create().inject(this)
+    }
+
+    override fun initObservers() {
+        super.initObservers()
+        observe(viewModel.leAccountAdd) {
+            goToUnlock()
+        }
+    }
+
+    private fun goToUnlock() {
+        replaceWithSlideLeft<UnlockView>()
+    }
+}
