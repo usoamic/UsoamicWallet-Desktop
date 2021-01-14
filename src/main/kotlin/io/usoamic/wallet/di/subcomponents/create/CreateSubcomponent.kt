04f73572f46df07e9d43c8bf6d96752c4ca6b166
primys <a2uso@tuta.io>
2021-01-14 14:53:02 -0800

    Add view for creating account

--- src/main/kotlin/io/usoamic/wallet/di/subcomponents/create/CreateSubcomponent.kt
+++ src/main/kotlin/io/usoamic/wallet/di/subcomponents/create/CreateSubcomponent.kt
@@ -2,11 +2,12 @@ package io.usoamic.wallet.di.subcomponents.create
 
 import dagger.Subcomponent
 import io.usoamic.wallet.di.scopes.AuthScope
+import io.usoamic.wallet.ui.auth.create.CreateView
 
 @AuthScope
 @Subcomponent
 interface CreateSubcomponent {
-    //fun inject(addFragment: CreateFragment)
+    fun inject(addFragment: CreateView)
 
     @Subcomponent.Factory
     interface Factory {
---
+++ src/main/kotlin/io/usoamic/wallet/ui/auth/create/CreateViewModel.kt
@@ -0,0 +1,28 @@
+package io.usoamic.wallet.ui.auth.create
+
+import io.usoamic.wallet.domain.models.ethereum.AccountCredentials
+import io.usoamic.wallet.extensions.addSchedulers
+import io.usoamic.wallet.other.MutableLiveData
+import io.usoamic.wallet.ui.base.BaseViewModel
+import io.usoamic.wallet.usecases.CreateAccountUseCases
+import javax.inject.Inject
+
+class CreateViewModel @Inject constructor(
+    mModel: CreateAccountUseCases
+) : BaseViewModel() {
+    val ldAddress = MutableLiveData<String>()
+    val ldPrivateKey = MutableLiveData<String>()
+
+    init {
+        mModel.createCredentials()
+            .addSchedulers()
+            .addProgress()
+            .subscribe(::setContent, ::throwErrorAndFinish)
+            .addToDisposable()
+    }
+
+    private fun setContent(credentials: AccountCredentials) {
+        ldAddress.value = credentials.address
+        ldPrivateKey.value = credentials.privateKey
+    }
+}
