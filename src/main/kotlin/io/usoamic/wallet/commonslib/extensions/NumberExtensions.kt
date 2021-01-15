397184292c9a54ddd125f9753f763e10f84c9cb6
primys <a2uso@tuta.io>
2021-01-15 14:10:33 -0800

    Add extension toBeautyString() for BigDecimal/BigInteger

--- src/main/kotlin/io/usoamic/wallet/commonslib/extensions/NumberExtensions.kt
+++ src/main/kotlin/io/usoamic/wallet/commonslib/extensions/NumberExtensions.kt
@@ -1,5 +1,14 @@
 package io.usoamic.wallet.commonslib.extensions
 
+import java.math.BigDecimal
 import java.math.BigInteger
 
 fun BigInteger?.orZero() = this ?: BigInteger.ZERO
+
+fun BigDecimal.toBeautyString(): String {
+    return stripTrailingZeros().toPlainString()
+}
+
+fun BigInteger.toBeautyString(): String {
+    return toString()
+}
