bf58386fbc669cb47d6ea4952ceb12394435933a
primys <a2uso@tuta.io>
2021-01-15 12:16:31 -0800

    Add UnlockView

--- src/main/kotlin/io/usoamic/wallet/di/AppComponent.kt
+++ src/main/kotlin/io/usoamic/wallet/di/AppComponent.kt
@@ -1,25 +1,28 @@
 package io.usoamic.wallet.di
 
 import dagger.Component
+import io.usoamic.wallet.di.modules.CompatModule
 import io.usoamic.wallet.di.modules.RepositoryModule
 import io.usoamic.wallet.di.modules.UsoamicModule
-import io.usoamic.wallet.di.scopes.AppScope
+import io.usoamic.wallet.commonslib.di.scopes.AppScope
 import io.usoamic.wallet.di.subcomponents.add.AddSubcomponent
 import io.usoamic.wallet.di.subcomponents.auth.AuthSubcomponent
 import io.usoamic.wallet.di.subcomponents.create.CreateSubcomponent
 import io.usoamic.wallet.di.subcomponents.dashboard.DashboardSubcomponent
 import io.usoamic.wallet.di.subcomponents.deposit.DepositSubcomponent
 import io.usoamic.wallet.di.subcomponents.history.HistorySubcomponent
+import io.usoamic.wallet.di.subcomponents.unlock.UnlockSubcomponent
 import io.usoamic.wallet.di.subcomponents.withdraw.WithdrawSubcomponent
 import io.usoamic.wallet.ui.start.StartView
 import javax.inject.Singleton
 
 @Singleton
 @AppScope
-@Component(modules = [UsoamicModule::class, RepositoryModule::class])
+@Component(modules = [UsoamicModule::class, RepositoryModule::class, CompatModule::class])
 interface AppComponent {
     val authSubcomponent: AuthSubcomponent
     val addSubcomponent: AddSubcomponent.Factory
+    val unlockSubcomponent: UnlockSubcomponent.Factory
 
     val createSubcomponent: CreateSubcomponent.Factory
     val dashboardSubcomponent: DashboardSubcomponent.Factory
---
+++ src/main/kotlin/io/usoamic/wallet/ui/auth/unlock/UnlockViewModel.kt
@@ -0,0 +1,46 @@
+package io.usoamic.wallet.ui.auth.unlock
+
+import io.usoamic.wallet.extensions.addSchedulers
+
+import io.usoamic.wallet.extensions.emit
+import io.usoamic.wallet.other.LiveEvent
+import io.usoamic.wallet.ui.base.BaseViewModel
+import io.usoamic.wallet.commonslib.usecases.UnlockUseCases
+import javax.inject.Inject
+
+class UnlockViewModel @Inject constructor(
+    private val mModel: UnlockUseCases
+) : BaseViewModel() {
+    val leNext = LiveEvent<Unit>()
+    val leLogout = LiveEvent<Boolean>()
+
+    fun onNextClick(password: String) {
+        mModel.getAddress(password)
+            .addSchedulers()
+            .addProgress()
+            .subscribe(::setData, ::throwError)
+            .addToDisposable()
+    }
+
+    fun onLogoutClick() {
+        mModel.removeAccount()
+            .addSchedulers()
+            .addProgress()
+            .subscribe({
+                onRemoveResult(true)
+            }, {
+                onRemoveResult(false)
+            })
+            .addToDisposable()
+    }
+
+    private fun onRemoveResult(isRemoved: Boolean) {
+        mModel.removePreferences()
+        leLogout.emit(isRemoved)
+    }
+
+    private fun setData(address: String) {
+        mModel.saveData(address)
+        leNext.emit()
+    }
+}
