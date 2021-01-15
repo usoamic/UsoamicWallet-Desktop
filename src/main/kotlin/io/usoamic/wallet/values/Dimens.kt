8aa2c1000ce53ab5a4d507357d59e14510f18358
primys <a2uso@tuta.io>
2021-01-15 14:10:06 -0800

    Add DEFAULT_LARGE_INDENT dimen

--- src/main/kotlin/io/usoamic/wallet/values/Dimens.kt
+++ src/main/kotlin/io/usoamic/wallet/values/Dimens.kt
@@ -4,6 +4,7 @@ object Dimens {
     const val DEFAULT_VERY_SMALL_INDENT = 1.5
     const val DEFAULT_SMALL_INDENT = 5.0
     const val DEFAULT_INDENT = 10.0
+    const val DEFAULT_LARGE_INDENT = 15.0
     const val DEFAULT_INDENT2 = 20.0
     const val DEFAULT_INDENT3 = 25.0
 }
