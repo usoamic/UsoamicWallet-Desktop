5a4f0c85eb505a3d5b4bbf3a7074dc46cfbba21e
primys <a2uso@tuta.io>
2021-01-15 11:38:21 -0800

    Move scopes from di folder to commonslib

--- src/main/kotlin/io/usoamic/wallet/commonslib/di/scopes/WalletScope.kt
+++ src/main/kotlin/io/usoamic/wallet/commonslib/di/scopes/WalletScope.kt
@@ -1,7 +1,7 @@
-package io.usoamic.wallet.di.scopes
+package io.usoamic.wallet.commonslib.di.scopes
 
 import javax.inject.Scope
 
 @Scope
 @kotlin.annotation.Retention(AnnotationRetention.RUNTIME)
-annotation class AuthScope
+annotation class WalletScope
