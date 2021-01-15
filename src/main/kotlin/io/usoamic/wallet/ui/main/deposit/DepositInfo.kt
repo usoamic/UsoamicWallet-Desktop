acbf0512e45365e14525df8f493a358a82bbee90
primys <a2uso@tuta.io>
2021-01-15 14:15:28 -0800

    Remove unused DepositInfo

--- src/main/kotlin/io/usoamic/wallet/ui/main/deposit/DepositInfo.kt
+++
@@ -1,9 +0,0 @@
-package io.usoamic.wallet.ui.main.deposit
-
-import java.awt.image.BufferedImage
-
-@Deprecated("Move to separate library")
-data class DepositInfo(
-    val address: String,
-    val qrCode: BufferedImage
-)
