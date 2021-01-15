30cb43621fb3487bf2737f9dc436f1caa6269048
primys <a2uso@tuta.io>
2021-01-15 14:09:54 -0800

    Rename getIcon() to getIconView() and add it for FontAwesomeIcon

--- src/main/kotlin/io/usoamic/wallet/util/FontAwesomeFXUtil.kt
+++ src/main/kotlin/io/usoamic/wallet/util/FontAwesomeFXUtil.kt
@@ -1,12 +1,21 @@
 package io.usoamic.wallet.util
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView
 import javafx.scene.paint.Color
 
 object FontAwesomeFXUtil {
     //https://github.com/edvin/tornadofx/issues/609
-    fun getIcon(icon: MaterialDesignIcon, color: Color, size: Int = 20): MaterialDesignIconView {
+    fun getIconView(icon: FontAwesomeIcon, color: Color, size: Int = 20): FontAwesomeIconView {
+        return FontAwesomeIconView(icon).apply {
+            glyphSize = size
+            fill = color
+        }
+    }
+
+    fun getIconView(icon: MaterialDesignIcon, color: Color, size: Int = 20): MaterialDesignIconView {
         return MaterialDesignIconView(icon).apply {
             glyphSize = size
             fill = color
