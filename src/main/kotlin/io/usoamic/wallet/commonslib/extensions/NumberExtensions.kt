437079bf35405d901d020762c574f366bf61a17a
primys <a2uso@tuta.io>
2021-01-16 14:19:33 -0800

    Add toBeautyString for Coin and refactoring

--- src/main/kotlin/io/usoamic/wallet/commonslib/extensions/NumberExtensions.kt
+++ src/main/kotlin/io/usoamic/wallet/commonslib/extensions/NumberExtensions.kt
@@ -1,14 +1,26 @@
 package io.usoamic.wallet.commonslib.extensions
 
+import io.usoamic.usoamickt.util.Coin
 import java.math.BigDecimal
 import java.math.BigInteger
 
-fun BigInteger?.orZero() = this ?: BigInteger.ZERO
+fun BigInteger?.orZero(): BigInteger = this ?: BigInteger.ZERO
 
-fun BigDecimal.toBeautyString(): String {
-    return stripTrailingZeros().toPlainString()
+fun Coin.toBeautyString(appendix: String = ""): String {
+    return toBigDecimal().toBeautyString(appendix)
 }
 
-fun BigInteger.toBeautyString(): String {
-    return toString()
+fun BigDecimal.toBeautyString(appendix: String = ""): String {
+    return buildString {
+        append(this@toBeautyString.stripTrailingZeros().toPlainString())
+        append(" ")
+        append(appendix)
+    }
+}
+
+fun BigInteger.toBeautyString(appendix: String = ""): String {
+    return buildString {
+        append(this@toBeautyString.toString())
+        append(appendix)
+    }
 }
