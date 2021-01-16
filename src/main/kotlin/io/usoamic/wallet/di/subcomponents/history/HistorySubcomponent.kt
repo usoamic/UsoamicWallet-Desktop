82c08ffdbd5c8aff6c3e5ddb3b47e6d1bc7381d8
primys <a2uso@tuta.io>
2021-01-16 14:20:02 -0800

    Add History screen

--- src/main/kotlin/io/usoamic/wallet/di/subcomponents/history/HistorySubcomponent.kt
+++ src/main/kotlin/io/usoamic/wallet/di/subcomponents/history/HistorySubcomponent.kt
@@ -2,11 +2,12 @@ package io.usoamic.wallet.di.subcomponents.history
 
 import dagger.Subcomponent
 import io.usoamic.wallet.commonslib.di.scopes.WalletScope
+import io.usoamic.wallet.ui.main.history.HistoryView
 
 @WalletScope
 @Subcomponent
 interface HistorySubcomponent {
-   // fun inject(historyFragment: HistoryFragment)
+    fun inject(historyView: HistoryView)
 
     @Subcomponent.Factory
     interface Factory {
---
+++ src/main/kotlin/io/usoamic/wallet/ui/main/history/HistoryViewModel.kt
@@ -0,0 +1,32 @@
+package io.usoamic.wallet.ui.main.history
+
+import io.usoamic.wallet.commonslib.models.history.TransactionItem
+import io.usoamic.wallet.commonslib.usecases.HistoryUseCases
+import io.usoamic.wallet.extensions.addSchedulers
+import io.usoamic.wallet.other.MutableLiveData
+import io.usoamic.wallet.ui.base.BaseViewModel
+import javax.inject.Inject
+
+class HistoryViewModel @Inject constructor(
+    private val mUseCases: HistoryUseCases
+) : BaseViewModel() {
+    val ldData = MutableLiveData<List<TransactionItem>>()
+
+    init {
+        updateTransactions()
+    }
+
+    fun onRefresh() = updateTransactions(true)
+
+    private fun updateTransactions(force: Boolean = false) {
+        mUseCases.getTransactions(force)
+            .addSchedulers()
+            .addProgress()
+            .subscribe(::setHistory, ::throwError)
+            .addToDisposable()
+    }
+
+    private fun setHistory(list: List<TransactionItem>) {
+        ldData.value = list
+    }
+}
