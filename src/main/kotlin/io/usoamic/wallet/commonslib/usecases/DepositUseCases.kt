8dfbfca7afe49d65f9fa7d7e85acdecc684194cb
primys <a2uso@tuta.io>
2021-01-16 10:32:01 -0800

    Now generateQrCode return BitMatrix

--- src/main/kotlin/io/usoamic/wallet/commonslib/usecases/DepositUseCases.kt
+++ src/main/kotlin/io/usoamic/wallet/commonslib/usecases/DepositUseCases.kt
@@ -1,12 +1,11 @@
 package io.usoamic.wallet.commonslib.usecases
 
 import com.google.zxing.BarcodeFormat
+import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
 import io.reactivex.Single
 import io.usoamic.wallet.commonslib.repositories.PreferencesRepository
 import io.usoamic.wallet.extensions.addDebugDelay
-import java.awt.Color
-import java.awt.image.BufferedImage
 import javax.inject.Inject
 
 class DepositUseCases @Inject constructor(
@@ -16,20 +15,10 @@ class DepositUseCases @Inject constructor(
         return mPreferencesRepository.getAddress()
     }
 
-    fun generateQrCode(content: String): Single<BufferedImage> {
+    fun generateQrCode(content: String): Single<BitMatrix> {
         return Single.fromCallable {
             val writer = QRCodeWriter()
-            val bitMatrix = writer.encode(content, BarcodeFormat.QR_CODE, 512, 512)
-            val width = bitMatrix.width
-            val height = bitMatrix.height
-
-            val bitmap = BufferedImage(width, height, BufferedImage.TYPE_USHORT_565_RGB)
-            for (x in 0 until width) {
-                for (y in 0 until height) {
-                    bitmap.setRGB(x, y, if (bitMatrix.get(x, y)) Color.BLACK.rgb else Color.WHITE.rgb)
-                }
-            }
-            bitmap
+            writer.encode(content, BarcodeFormat.QR_CODE, 512, 512)
         }
             .addDebugDelay()
     }
