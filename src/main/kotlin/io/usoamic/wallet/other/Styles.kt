af990a2f177e1e72e2dedba08def01f9bc93210c
primys <a2uso@tuta.io>
2021-01-16 10:32:40 -0800

    Change window size

--- src/main/kotlin/io/usoamic/wallet/other/Styles.kt
+++ src/main/kotlin/io/usoamic/wallet/other/Styles.kt
@@ -2,21 +2,26 @@ package io.usoamic.wallet.other
 
 import io.usoamic.wallet.util.ResourcesUtil
 import io.usoamic.wallet.values.R
-import javafx.scene.text.FontWeight
 import tornadofx.*
 
 class Styles : Stylesheet() {
-    companion object {
-        val heading by cssclass()
-    }
-
     init {
-        label and heading {
-            padding = box(10.px)
-            fontSize = 20.px
-            fontWeight = FontWeight.BOLD
+        root {
+            prefWidth = WIDTH.px
+            prefHeight = HEIGHT.px
+
+            minWidth = 0.px
+            minHeight = 0.px
+
+            maxWidth = WIDTH.px
+            maxHeight = HEIGHT.px
         }
 
         setStageIcon(ResourcesUtil.getImage(R.image.IC_LAUNCHER))
+    }
+
+    companion object {
+        const val WIDTH = 400.0
+        const val HEIGHT = 400.0
     }
 }
