65a252e1a9dea76e245dd8406264e0b72a81cd8d
primys <primys@usoamic.io>
2021-12-06 05:26:02 +0300

    Bump version

--- src/main/kotlin/io/usoamic/wallet/util/BuildConfig.kt
+++ src/main/kotlin/io/usoamic/wallet/util/BuildConfig.kt
@@ -4,9 +4,9 @@ object BuildConfig {
     const val DEBUG = false
     const val APPLICATION_ID = "io.usoamic.wallet"
     const val BUILD_TYPE = "release"
-    const val FLAVOR = "MainNet"
-    const val VERSION_CODE = 3
-    const val VERSION_NAME = "1.0.2"
+    const val FLAVOR = "MainNet" //MainNet
+    const val VERSION_CODE = 4
+    const val VERSION_NAME = "1.1.0"
 
     const val ACCOUNT_FILENAME = "account.json"
     const val DATE_FORMAT = "yyyy-MM-dd HH:mm"
